7f7a296dab4b13ac22ba386fafb62d3a4e47823b
fxiao1 <fx309@qq.com>
2019-03-28 20:46:49 +0800

    增加多对多模块的用户对象

---
+++ src/main/java/com/fx/modules/hibernateManyToMany/controller/UserCon.java
@@ -0,0 +1,57 @@
+package com.fx.modules.hibernateManyToMany.controller;
+
+import com.fx.modules.common.controller.BaseDataController;
+import com.fx.modules.common.tools.DataPack;
+import com.fx.modules.common.tools.EntityUtils;
+import com.fx.modules.hibernateManyToMany.entity.User;
+import com.fx.modules.hibernateManyToMany.service.UserSer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * TODO:
+ *
+ * @author fxiao
+ * @date 2019/3/28 20:29
+ */
+@RestController
+public class UserCon implements BaseDataController<User,Long> {
+    public UserCon(){super();}
+    private UserSer ser;
+    @Autowired
+    public UserCon(UserSer ser){
+        this.ser=ser;
+    }
+    @Override
+    @PostMapping("users")
+    public DataPack<User> save(User user) {
+        return ser.save(user);
+    }
+
+    @Override
+    @DeleteMapping("users/{id}")
+    public DataPack<User> deleteById(@PathVariable("id") Long id) {
+        ser.deleteById(id);
+        return EntityUtils.succ(null);
+    }
+
+
+    @Override
+    @GetMapping("users/{id}")
+    public DataPack<User> findById(@PathVariable("id") Long id) {
+        return ser.findById(id);
+    }
+
+    @Override
+    @GetMapping("users")
+    public DataPack<List<User>> findAll() {
+        return ser.findAll();
+    }
+
+    @Override
+    public DataPack<User> updata(User user) {
+        return ser.update(user);
+    }
+}
--- src/main/java/com/fx/modules/hibernateManyToMany/service/impl/UserSerImpl.java
+++ src/main/java/com/fx/modules/hibernateManyToMany/service/impl/UserSerImpl.java
@@ -1,48 +1,66 @@
 package com.fx.modules.hibernateManyToMany.service.impl;
 
 import com.fx.modules.common.tools.DataPack;
+import com.fx.modules.common.tools.EntityUtils;
 import com.fx.modules.hibernateManyToMany.dao.UserDao;
 import com.fx.modules.hibernateManyToMany.entity.User;
 import com.fx.modules.hibernateManyToMany.service.UserSer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
- * TODO:
+ * TODO: 用户对象的ser层
  *
  * @author fxiao
  * @date 2019/3/28 20:10
  */
+@Service
 public class UserSerImpl implements UserSer {
     private UserDao dao;
     @Autowired
     public UserSerImpl(UserDao dao){
         this.dao=dao;
-
     }
+    public UserSerImpl(){};
     @Override
-    public DataPack<User> findById(Long aLong) {
-        return null;
+    public DataPack<User> findById(Long id) {
+        Optional<User> optionalUser=dao.findById(id);
+        User user=new User();
+        if(optionalUser.isPresent()){
+            user=optionalUser.get();
+            return EntityUtils.succ(user);
+        }else{
+            return EntityUtils.error("不知道为啥，没有获取到用户");
+        }
     }
 
     @Override
     public DataPack<List<User>> findAll() {
-        return null;
+        return EntityUtils.succ(dao.findAll());
     }
 
     @Override
     public DataPack<User> save(User user) {
-        return null;
+        user.setId(0L);
+        dao.save(user);
+        return EntityUtils.succ(user);
     }
 
     @Override
     public DataPack<User> update(User user) {
-        return null;
+        if(user.getId()!=null&&user.getId()>0L){
+            dao.save(user);
+            return EntityUtils.succ(user);
+        }else{
+            return EntityUtils.error("更新失败：未找到对象id");
+        }
     }
 
     @Override
-    public void deleteById(Long aLong) {
-
+    public void deleteById(Long id) {
+        dao.deleteById(id);
     }
 }
